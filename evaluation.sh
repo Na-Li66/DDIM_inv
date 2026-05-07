e2712b92e322279f4a1dd79dddee8d5b3beeabdb
Na Li <113565640+Na-Li66@users.noreply.github.com>
2026-05-07 15:46:12 +0800

    Update evaluation.sh
    Updated the deg_scale parameter to 5.0 for various configurations in the evaluation script.

--- evaluation.sh
+++ evaluation.sh
@@ -11,13 +11,13 @@ python main.py --config celeba-hq.yml --path_y celeba/face --eta 0.0 16.0 --add_
 python main.py --config celeba-hq.yml --path_y celeba/face --eta 0.0 16.0 --add_noise --deg "inpainting" --deg_scale 4.0 --sigma_y 0.05 --NFE 100 --ntrials 4 -i demo
 
 # Table 5:
-python main.py --config imagenet_256.yml --path_y imagenet --eta 0.0 16.0 --add_noise --deg "deblur_gauss" --deg_scale 4.0 --sigma_y 0.05 --NFE 100 --ntrials 1 -i demo
-python main.py --config imagenet_256.yml --path_y imagenet --eta 0.0 16.0 --add_noise --deg "deblur_gauss" --deg_scale 4.0 --sigma_y 0.05 --NFE 100 --ntrials 4 -i demo
-python main.py --config celeba-hq.yml --path_y celeba/face --eta 0.0 16.0 --add_noise --deg "deblur_gauss" --deg_scale 4.0 --sigma_y 0.05 --NFE 100 --ntrials 1 -i demo
-python main.py --config celeba-hq.yml --path_y celeba/face --eta 0.0 16.0 --add_noise --deg "deblur_gauss" --deg_scale 4.0 --sigma_y 0.05 --NFE 100 --ntrials 4 -i demo
+python main.py --config imagenet_256.yml --path_y imagenet --eta 0.0 16.0 --add_noise --deg "deblur_gauss" --deg_scale 5.0 --sigma_y 0.05 --NFE 100 --ntrials 1 -i demo
+python main.py --config imagenet_256.yml --path_y imagenet --eta 0.0 16.0 --add_noise --deg "deblur_gauss" --deg_scale 5.0 --sigma_y 0.05 --NFE 100 --ntrials 4 -i demo
+python main.py --config celeba-hq.yml --path_y celeba/face --eta 0.0 16.0 --add_noise --deg "deblur_gauss" --deg_scale 5.0 --sigma_y 0.05 --NFE 100 --ntrials 1 -i demo
+python main.py --config celeba-hq.yml --path_y celeba/face --eta 0.0 16.0 --add_noise --deg "deblur_gauss" --deg_scale 5.0 --sigma_y 0.05 --NFE 100 --ntrials 4 -i demo
 
 # Table 6:
-python main.py --config imagenet_256.yml --path_y imagenet --eta 0.0 16.0 --add_noise --deg "cs_walshhadamard" --deg_scale 4.0 --sigma_y 0.05 --NFE 100 --ntrials 1 -i demo
-python main.py --config imagenet_256.yml --path_y imagenet --eta 0.0 16.0 --add_noise --deg "cs_walshhadamard" --deg_scale 4.0 --sigma_y 0.05 --NFE 100 --ntrials 4 -i demo
-python main.py --config celeba-hq.yml --path_y celeba/face --eta 0.0 16.0 --add_noise --deg "cs_walshhadamard" --deg_scale 4.0 --sigma_y 0.05 --NFE 100 --ntrials 1 -i demo
-python main.py --config celeba-hq.yml --path_y celeba/face --eta 0.0 16.0 --add_noise --deg "cs_walshhadamard" --deg_scale 4.0 --sigma_y 0.05 --NFE 100 --ntrials 4 -i demo
+python main.py --config imagenet_256.yml --path_y imagenet --eta 0.0 16.0 --add_noise --deg "cs_walshhadamard" --deg_scale 5.0 --sigma_y 0.05 --NFE 100 --ntrials 1 -i demo
+python main.py --config imagenet_256.yml --path_y imagenet --eta 0.0 16.0 --add_noise --deg "cs_walshhadamard" --deg_scale 5.0 --sigma_y 0.05 --NFE 100 --ntrials 4 -i demo
+python main.py --config celeba-hq.yml --path_y celeba/face --eta 0.0 16.0 --add_noise --deg "cs_walshhadamard" --deg_scale 5.0 --sigma_y 0.05 --NFE 100 --ntrials 1 -i demo
+python main.py --config celeba-hq.yml --path_y celeba/face --eta 0.0 16.0 --add_noise --deg "cs_walshhadamard" --deg_scale 5.0 --sigma_y 0.05 --NFE 100 --ntrials 4 -i demo
